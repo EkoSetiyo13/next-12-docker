2951be7965ed48efaad0a77cd74c62d17145675a
Eko Setiyo Budi Purnomo <LTL002742@lautan-luas.com>
2024-10-24 15:59:38 +0700

    feat : update profile

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,13 +1,20 @@
 'use client'
 
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import { Interceptor } from 'src/utils/env';
 import styles from '../../../styles/Home.module.css'
 
 export const dynamic = "force-dynamic";
 
 export default function Profile() {
-   
+    const [intern, setIntern] = useState("")
+    useEffect(() => { handleData() }, [])
+    const handleData = async () => {
+        const data = await Interceptor()
+        console.log("data", data)
+        setIntern(data?.env)
+    }
     return (
         <div className={styles.container}>
             <Head>
@@ -15,7 +22,7 @@ export default function Profile() {
             </Head>
 
             <main className={styles.main}>
-                <h1>Profile Page : {process.env.NEXT_PUBLIC_API_URL}</h1>
+                <h1>Profile Page : {process.env.NEXT_PUBLIC_API_URL} || {intern}</h1>
                 <a href="/">Back to Home</a>
             </main>
         </div>
