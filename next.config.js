85634f3bd639fcd367b49df5a66484c412112aa1
Eko Setiyo Budi Purnomo <LTL002742@lautan-luas.com>
2024-10-24 12:55:29 +0700

    feat : add NEXT_PUBLIC

--- next.config.js
+++ next.config.js
@@ -7,12 +7,18 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
     BASE_URL: process.env.BASE_URL,
-    APP_ENV: process.env.APP_ENV
+    APP_ENV: process.env.APP_ENV,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
     BASE_URL: process.env.BASE_URL,
-    APP_ENV: process.env.APP_ENV
+    APP_ENV: process.env.APP_ENV,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   webpack: (config) => {
     return config
